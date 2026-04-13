dea20603b24a10db46fb4277c943724f2474bea0
aisyahd01 <aisyahdahayu02@gmail.com>
2026-04-13 08:57:39 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,53 @@
+//Nama: Aisyah Dahayu Shabirah
+//NIM: 13224059
+//Soal 1
+
 #include <stdio.h>
 
-int main(void) {
+int main (){
+    int izin, jam;
+    int suhu, radiasi;
+    scanf("%d", &izin);
+    scanf("%d", &suhu);
+    scanf("%d", &radiasi);
+    scanf("%d", &jam);
+
+    if (izin==1){
+        if (jam<6 || jam>20){
+            printf("TOLAK");
+        }
+        else if (radiasi>=6){
+            printf("TOLAK");
+        }
+        else {
+        printf("MASUK");}
+        }
+        
+    else if (izin==2){ 
+        if (radiasi <=2 && jam>=8 && jam <=18){
+            printf("MASUK");}
+        else if (radiasi>=6){
+            printf("TOLAK");
+        }
+        else {
+            printf("PEMERIKSAAN");}
+        }
+    else if (izin==3){
+        if( radiasi==0 && suhu<380){
+            printf("MASUK");}
+        else if (radiasi>=6){
+            printf("TOLAK");
+        }
+    else {
+        printf("TOLAK");
+    }}
     return 0;
 }
+        
+
+
+
+
+
+
+
